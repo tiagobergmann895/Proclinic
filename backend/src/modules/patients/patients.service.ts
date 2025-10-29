b0b66572dcf3fb4274ecb0837b2b328bb2c49e38
Tiago <tiagobergmann@gmail.com>
2025-10-29 19:28:03 -0300

    Fix TypeScript errors

--- backend/src/modules/patients/patients.service.ts
+++ backend/src/modules/patients/patients.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../common/prisma.service';
 import { Prisma } from '@prisma/client';
-
 @Injectable()
 export class PatientsService {
   constructor(private readonly prisma: PrismaService) {}
