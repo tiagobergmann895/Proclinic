86bf0bc2ec3ed8c781962c1c3cad4557b1b4bf2e
Tiago <tiagobergmann@gmail.com>
2025-10-29 19:51:55 -0300

    Update app.module.ts

--- backend/src/modules/app.module.ts
+++ backend/src/modules/app.module.ts
@@ -1,35 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from '../common/prisma.module';
-import { InventoryModule } from './inventory/inventory.module';
-import { CostingModule } from './costing/costing.module';
-import { PricingModule } from './pricing/pricing.module';
-import { ProceduresModule } from './procedures/procedures.module';
-import { AuthModule } from './auth/auth.module';
-import { PatientsModule } from './patients/patients.module';
-import { ItemsModule } from './items/items.module';
-import { ProcedureTypesModule } from './procedure-types/procedure-types.module';
-import { ReportsModule } from './reports/reports.module';
-import { SubscriptionsModule } from './subscriptions/subscriptions.module';
-import { EhrModule } from './ehr/ehr.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { PatientsModule } from './modules/patients/patients.module';
+import { AppointmentsModule } from './modules/appointments/appointments.module';
+import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
+// import { EhrModule } from './modules/ehr/ehr.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule,
-    InventoryModule,
-    CostingModule,
-    PricingModule,
     AuthModule,
+    UsersModule,
     PatientsModule,
-    ItemsModule,
-    ProcedureTypesModule,
-    ProceduresModule,
-    ReportsModule,
+    AppointmentsModule,
     SubscriptionsModule,
-    EhrModule,
+    // EhrModule,
   ],
 })
 export class AppModule {}
-
-
