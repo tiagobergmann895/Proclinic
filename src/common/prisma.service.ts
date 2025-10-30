d90b11086c47e826d8843c1167deb34d828f7797
Tiago <tiagobergmann@gmail.com>
2025-10-30 18:25:08 -0300

    Fix prisma service syntax

--- src/common/prisma.service.ts
+++ src/common/prisma.service.ts
@@ -4,6 +4,6 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit(): Promise<void> {
-    await this.();
+    await this.$connect();
   }
 }
