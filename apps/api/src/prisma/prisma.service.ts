2e7cb33e9fe87111cf14d3a2d0dc40441fc3ac82
Tiago <tiagobergmann@gmail.com>
2026-03-05 09:34:49 -0300

    fix: corrigir PrismaService duplicado, adicionar scripts de produção e prisma generate na build

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -2,5 +2,8 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-@Injectable()
-export class PrismaService extends PrismaClient { }
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    async onModuleInit() {
+        await this.$connect();
+    }
+}
